e73c5c722a6c11a63324674795ae67381924fc51
Nirajan <nirajan@whitehatengineering.com>
2019-08-22 11:15:53 +0545

    initiate app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mymaison::Application.config.secret_key_base = '34a2b070f361b51d7de7aa53b67e3e34f88b30a09aa055941c34e341f764b62f9b27738545270556bc799f53f464ee9867a7d5c3149e91daa825d94356004779'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mymaison::Application.config.session_store :cookie_store, key: '_mymaison_session'
