13f89d106b9646d806d1e4ec02ea06a4574eddcd
Nirajan <nirajan@whitehatengineering.com>
2019-08-22 11:36:09 +0545

    add Property model to store properties the user owns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190822053558) do
+ActiveRecord::Schema.define(version: 20190822055016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20190822053558) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "properties", force: true do |t|
+    t.string   "name"
+    t.string   "location"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "properties", ["user_id"], name: "index_properties_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
